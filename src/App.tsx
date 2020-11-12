0e29a99e46c14611cb197d67ac1cc8816c5b40c7
Hongjia Liu <hongjia_liu@outlook.com>
2020-11-12 02:34:07 +0000

    react-router-dom initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,67 @@
 import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">Tags</Link>
+            </li>
+            <li>
+              <Link to="/track">Track</Link>
+            </li>
+            <li>
+              <Link to="/statistics">Statistics</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/track">
+            <Track/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/track" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return (
+    <div>404</div>
+  )
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Track() {
+  return <h2>Track</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
 }
 
 export default App;
