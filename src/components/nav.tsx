570086b8bc01fb5f10ae02e0e75d530b10023a62
Hongjia Liu <hongjia_liu@outlook.com>
2020-11-12 15:26:36 +0000

    Updated navigation bar icons

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/euro.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -10,7 +14,14 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -20,12 +31,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">Tags</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#euro" />
+          </svg>
           <Link to="/track">Track</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/statistics">Statistics</Link>
         </li>
       </ul>
